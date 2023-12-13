263cac04bcc35c7fce31769802af458435bacd1a
PanavMhatre <mhatrepanav@gmail.com>
2023-12-12 18:26:25 -0600

    Worked on Navbar and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,13 @@ function Footer() {
 
 <section className="relative bg-blueGray-200 pt-8 pb-6">
     <div className="my-6 border-blueGray-300">
+    <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"/>
+
     <div className="flex flex-wrap items-center md:justify-between justify-center">
       <div className="w-full md:w-4/12 px-4 mx-auto text-center">
         <div className="text-sm text-blueGray-500 font-semibold py-1">
           Copyright © <span id="get-current-year">2023</span> <a href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 hover:text-gray-800" target="_blank"> Panav Mhatre</a>
-          <a href="https://www.creative-tim.com?ref=njs-profile" className="text-blueGray-500 hover:text-blueGray-800"></a>
+          
         </div>
       </div>
     </div>
